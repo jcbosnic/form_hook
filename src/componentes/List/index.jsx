57b946fc2453c8f60fb9d24ab95b680074c72107
jcbosnic <jcbosnic@gmail.com>
2022-08-30 16:24:10 -0400

    Primer commit

---
+++ src/componentes/List/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const List = () => (
+    <ul>
+        <li>Aprender React</li>
+        <li>Aprender Mongo</li>
+        <li>Aprender styles-components</li>
+        <li>Aprender API Rest</li>
+    </ul>
+)
+
+export default List;
---
+++ src/componentes/Titles/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Title = () => (
+    <div>
+        <h1>Hello Dojo!!</h1>
+        <h3>Esta es mi lista de tareas:</h3>   
+    </div>    
+)
+
+export default Title;
